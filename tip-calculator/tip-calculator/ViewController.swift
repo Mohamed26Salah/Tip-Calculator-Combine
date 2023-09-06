be89b2d8d9863bb49431551dc74fb8ecf3735c61
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2023-09-06 17:30:08 +0300

    Init

---
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Mohamed Salah on 06/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
